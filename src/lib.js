03c06c095de50d5d675b91307e98fc4c7bb1b300
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-10-25 20:15:21 +0530

    Removing unnecessary if from lib.

--- src/lib.js
+++ src/lib.js
@@ -1,14 +1,14 @@
 const makeConstant = function(value) {
   return function() {
     const result = value;
-    return value;
+    return result;
   }
 }
 
 const makeCounterFromN = function(countStart) {
-  let count=0;
+  let count = 0;
   return function() {
-    count=countStart;
+    count = countStart;
     countStart++;
     return count;
   }
@@ -20,43 +20,39 @@ const makeCounterFromZero = function() {
 
 const makeDeltaTracker = function(oldKeyValue) {
   return function(deltaCurrentValue) {
-    if(deltaCurrentValue == undefined ) {
-      deltaCurrentValue = 0;
-    }
-    let record={old:oldKeyValue };
+    deltaCurrentValue = (deltaCurrentValue || 0);
+    let record = { old : oldKeyValue };
     record["delta"] = deltaCurrentValue;
     record["new"] = record["delta"]+record["old"];
-    oldKeyValue=record["new"];
+    oldKeyValue = record["new"];
     return record;
   }
 }
 
-const makeFiboGenerator = function(firstTerm,secTerm) {
-  if(firstTerm == undefined && secTerm == undefined){
+const makeFiboGenerator = function(firstTerm,secondTerm) {
+  if(firstTerm == undefined && secondTerm == undefined) {
     firstTerm = 0;
-    secTerm = 1;
+    secondTerm = 1;
   }
-  if(secTerm == undefined && firstTerm != undefined) {
-    secTerm = firstTerm;
+  if(secondTerm == undefined && firstTerm != undefined) {
+    secondTerm = firstTerm;
     firstTerm = 0;
   }
   let result = firstTerm;
   return function(){
     firstTerm = result;
-    result = secTerm;
-    secTerm = firstTerm + secTerm;
+    result = secondTerm;
+    secondTerm = firstTerm + secondTerm;
     return firstTerm;
   }
 }
 
 const makeCycler = function(source) {
   let index = 0;
-  source = source.slice();
+  sourceCopy = source.slice();
   return function() {
-    if(index >= source.length) {
-      index = 0;
-    }
-    let result = source[index];
+    index = (index % sourceCopy.length);
+    let result = sourceCopy[index];
     index++;
     return result;
   }
